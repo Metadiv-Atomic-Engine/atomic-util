1cb510bcdc414bcdf56ff3b2bf354fb96ae8fb3f
Nelson Lai <nelsonlai9503@gmail.com>
2024-03-11 16:56:40 +0800

    system support image

--- setting/model/entity/SystemSetting.go
+++ setting/model/entity/SystemSetting.go
@@ -2,8 +2,8 @@ package entity
 
 type SystemSetting struct {
 	Key         string `gorm:"primaryKey;not null" json:"key"`
-	Type        string `gorm:"not null" json:"type"` // string, int, float, bool
-	Value       string `json:"value"`
-	WorkspaceID uint   `json:"workspace_id"` // 0 means global settings
+	Type        string `gorm:"not null" json:"type"` // string, int, float, bool, image
+	Value       string `json:"value"`                // if the type is image, the value is the image uuid
+	WorkspaceID uint   `json:"workspace_id"`         // 0 means global settings
 	Public      bool   `json:"public"`
 }
