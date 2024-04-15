aaac911168ab3c6fb2be56156eed0e1852b2e91a
Nelson Lai <nelsonlai9503@gmail.com>
2024-04-15 13:39:25 +0800

    fix template delete

--- smtp/handler/api-smtp-template-delete.go
+++ smtp/handler/api-smtp-template-delete.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"github.com/Metadiv-Atomic-Engine/atomic-util/smtp/errs"
 	"github.com/Metadiv-Atomic-Engine/atomic-util/smtp/module"
+	"github.com/Metadiv-Atomic-Engine/atomic-util/smtp/repo"
 	"github.com/Metadiv-Atomic-Engine/atomic/atomic"
 	"github.com/Metadiv-Atomic-Engine/atomic/base"
 )
@@ -11,7 +13,17 @@ var API_SMTP_TEMPLATE_DELETE = atomic.NewApiHandler(
 	"6YsHyWg0stBUGYZ65XTFf",
 	"Delete SMTP template",
 	func(ctx *atomic.Context[base.RequestIDPath]) {
+		template := repo.SmtpTemplateRepo.FindByID(ctx.DB, ctx.Request.ID, ctx.WorkspaceID())
+		if template == nil {
+			ctx.Err(errs.SMTP_TEMPLATE_NOT_FOUND)
+			return
+		}
 
+		if !repo.SmtpTemplateRepo.Delete(ctx.DB, template) {
+			ctx.InternalServerErr("Failed to delete template")
+			return
+		}
+		ctx.OK(nil)
 	},
 	&atomic.TypescriptOpt{
 		FunctionName: "deleteSmtpTemplate",
