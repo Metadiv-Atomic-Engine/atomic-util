f8703c55487c2b44081a4309a2df68285c560b6d
Nelson Lai <nelsonlai9503@gmail.com>
2024-04-20 21:11:13 +0800

    refactor list static

--- static/handler/api-static-list.go
+++ static/handler/api-static-list.go
@@ -1,11 +1,13 @@
 package handler
 
 import (
+	"strings"
+
 	"github.com/Metadiv-Atomic-Engine/atomic-util/static/model/entity"
+	"github.com/Metadiv-Atomic-Engine/atomic-util/static/model/request"
 	"github.com/Metadiv-Atomic-Engine/atomic-util/static/module"
 	"github.com/Metadiv-Atomic-Engine/atomic-util/static/repo"
 	"github.com/Metadiv-Atomic-Engine/atomic/atomic"
-	"github.com/Metadiv-Atomic-Engine/atomic/base"
 	"github.com/Metadiv-Atomic-Engine/sql"
 )
 
@@ -13,15 +15,19 @@ var API_STATIC_LIST = atomic.NewApiHandler(
 	module.Static,
 	"UI7cPHeyg4tg2T3yf0WEP",
 	"List static",
-	func(ctx *atomic.Context[base.RequestListing]) {
-		statics, page := repo.StaticRepo.FindAllComplex(ctx.DB, sql.And(
-			ctx.Request.BuildSimilarClause(
-				"uuid",
-				"filename",
-				"file_type",
-			),
-			sql.Eq("pinned", true),
-		), ctx.Page, ctx.Sort, "", ctx.WorkspaceID())
+	func(ctx *atomic.Context[request.StaticList]) {
+		cls := make([]*sql.Clause, 0)
+		cls = append(cls, sql.Eq("pinned", true))
+		cls = append(cls, ctx.Request.BuildSimilarClause(
+			"uuid",
+			"filename",
+			"file_type",
+		))
+		if ctx.Request.UUIDs != "" {
+			cls = append(cls, sql.In("uuid", strings.Split(ctx.Request.UUIDs, ",")))
+		}
+		statics, page := repo.StaticRepo.FindAllComplex(ctx.DB,
+			sql.And(cls...), ctx.Page, ctx.Sort, "", ctx.WorkspaceID())
 		if statics == nil {
 			ctx.InternalServerErr()
 			return
---
+++ static/model/request/StaticList.go
@@ -0,0 +1,8 @@
+package request
+
+import "github.com/Metadiv-Atomic-Engine/atomic/base"
+
+type StaticList struct {
+	base.RequestListing
+	UUIDs string `form:"uuids"` // split by comma
+}
